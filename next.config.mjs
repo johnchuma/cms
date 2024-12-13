24e1b0dbf48a2b4cfa7e509e64d13eac0d173e2b
johnchuma <johnvchuma@gmail.com>
2024-12-13 16:30:06 +0300

    host next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-      },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: false, // 'minify' in Next versions < 12.0
 };
 
 export default nextConfig;
